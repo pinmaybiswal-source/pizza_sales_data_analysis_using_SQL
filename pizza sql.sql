3ac640445a7a295f5fdad4102381539ce020c644
pinmaybiswal-source <pinmaybiswal@gmail.com>
2025-12-19 11:05:16 +0530

    SQL queries for pizza database

--- pizza sql.sql
+++ pizza sql.sql
@@ -1,60 +1,44 @@
+-- Creating a Database as pizzahut
 create database pizzahut;
 use pizzahut;
 
-
+-- Import data as CSV file
+-- Retrieve the all records from an pizzas table.
 select * from pizzas;
+-- Retrieve the all records from an pizza_types table.
 select * from pizza_types;
 
+-- creating table for orders
 create table Orders(order_id int not null,
 order_Date date not null ,order_time time not null,
 primary key(order_id));
+-- Retrieve the top 10 records from an Orders table.
 select * from Orders limit 10;
 
+-- Creating table for Order_details
 create table Order_details(order_details_id int not null, order_id int not null,
 pizza_id text not null ,quantity int not null,
 primary key(order_details_id));
-
+-- Retrieve the top 10 records from an Order_details table.
 select * from Order_details limit 10;
 
+-- Retrieve the total number of orders placed.
+select count(order_id) as total_order from Orders;
 
+-- Calculate the total revenue generated from pizza sales.
+SELECT round(SUM(p.price * od.quantity),2) AS Total_Revenue
+FROM pizzas p JOIN order_details od ON p.pizza_id = od.pizza_id; 
 
-
-select count(order_id) from Orders;
-
-SELECT 
-    round(SUM(p.price * od.quantity),2) AS Total_Revenue
-FROM
-    pizzas p
-        JOIN
-    order_details od ON p.pizza_id = od.pizza_id; 
-
-
-
+-- Identify the highest-priced pizza.
 select t.name , t.category , p.price from pizza_types t
 join pizzas p  on p.pizza_type_id = t.pizza_type_id 
 order by p.price  desc limit 1;
 
-
-
-
-
-select p.size , (p.price * od.quantity) total_price
-from pizzas p
-join order_details od on p.pizza_id = od.pizza_id
-order by total_price desc limit 1;
-
-
-
-
-
-
-
+-- Identify the most common pizza size ordered.
 select p.size,count(od.order_details_id) quantity from pizzas p join order_details od 
 on p.pizza_id = od.pizza_id
 group by p.size 
 order by quantity desc  limit 1;
-
-
 
 -- List the top 5 most ordered pizza types along with their quantities.
 select t.name ,sum(od.quantity) quantity from pizzas p
@@ -71,10 +55,8 @@ join  order_details od on p.pizza_id = od.pizza_id
 group by t.category;
 
 -- Determine the distribution of orders by hour of the day.
-
 select hour(order_time) By_hour ,count(order_id) from orders 
 group by by_hour ;
-
 
 -- Join relevant tables to find the category-wise distribution of pizzas.
 select t.category , count(name) from pizza_types t  
@@ -88,8 +70,6 @@ group by o.order_date) order_quantity;
 
 
 -- Determine the top 3 most ordered pizza types based on revenue.
-
-
 select t.name,sum(p.price*od.quantity) Total_Reveune from pizza_types t
 join pizzas p on t.pizza_type_id=p.pizza_type_id
 join order_details od on p.pizza_id=od.pizza_id
@@ -97,7 +77,6 @@ group by t.name
 order by Total_Reveune desc limit 3 ;
 
 -- Calculate the percentage contribution of each pizza type to total revenue.
-
 SELECT t.category,ROUND(SUM(p.price * od.quantity) /
 (SELECT SUM(p.price * od.quantity) total_sale 
 FROM order_details od JOIN pizzas p ON od.pizza_id = p.pizza_id) * 100,2) AS revenue 
